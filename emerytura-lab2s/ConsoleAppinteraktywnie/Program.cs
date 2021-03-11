e7f5328aa51c061dace43806f3be1a917bc1d03f
itstomekw <80002511+itstomekw@users.noreply.github.com>
2021-03-11 09:37:42 +0100

    korekty

--- emerytura-lab2s/ConsoleAppinteraktywnie/Program.cs
+++ emerytura-lab2s/ConsoleAppinteraktywnie/Program.cs
@@ -20,8 +20,20 @@ namespace ConsoleAppinteraktywnie
 
             Console.WriteLine("Witaj, {0} {1}!", imie, nazwisko");
 
-                Console.WriteLine($"Wtaj, {imie} {nazwisko}!");
+            Console.WriteLine($"Wtaj, {imie} {nazwisko}!");
 
+            string wiekTekstowo = Console.ReadLine();
+            int wiek = int.Parse(wiekTekstowo)
+
+            if (wiek > 65)
+            {
+                Console.WriteLine("Jesteś emerytem");
+
+            }
+            else
+            {
+                Console.WriteLine("Do emerytury zostało Ci" {wiek - 75 }"lat");
+            }
         }
     }
 }
